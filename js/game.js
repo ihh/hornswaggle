c62f100315e763996d9bffcdd1bccfbf228cc64a
Ian Holmes <yam@ians-mbp-2.gateway.sonic.net>
2023-04-07 15:20:19 -0700

    meters

---
+++ js/game.js
@@ -0,0 +1,26 @@
+let c, d
+$(document).ready (() => {
+  c = new Carder ({ iconPrefix: 'img/' })
+  const config = {
+    carder: c,
+    debug: true,
+    meterIconPrefix: 'meters/',
+    meters: [{ name: 'food' },
+	     { name: 'fame' },
+	     { name: 'gold' }],
+    cards: [{ html: 'hello', priority: 2, limit: 1, left: { reward: { coins: .1 } } },
+	    { html: 'world', priority: 1, limit: 1 },
+	    { when: 'start',
+	      cssClass: 'title',
+	      cards: [{ html: 'filler', cool: 1 },
+		      { html: 'test card',
+			limit: 2,
+			cool: 1,
+			left: { scaledReward: { coins: .1 }, sequence: { cssClass: 'bonus', cards: [{cssClass:"warning",html:"one"},["two",2],"three"] } },
+			right: { stage: 'muppet', scaledReward: { coins: -.1 }, reward: { castle: .2 } } }] },
+	    { html: 'Time passes...', priority: -1 }],
+    status: (gs) => `Coins=${gs.coins} Castle=${gs.castle}`
+  }
+  d = new Dealer (config)
+  d.dealFirstCard()
+})
